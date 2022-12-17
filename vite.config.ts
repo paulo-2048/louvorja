787541779b21bc05cf1c7b2206c63c9beaa5d095
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2022-12-16 21:54:54 -0300

    Add Electron
    Rebasing branch 'feature/vuejs3' on '2e4a12f'.
    Changes to be committed:
    	new file:   electron/index.html
    	new file:   electron/index.js
    	new file:   electron/preload.js
    	modified:   package.json
    	modified:   vite.config.ts
    	new file:   yarn.lock

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from "node:url";
+import path from "path";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: path.resolve(__dirname, "./dist/").replace('\\', '/'),
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
